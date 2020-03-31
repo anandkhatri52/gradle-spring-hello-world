9c67c0c4611ac421ebc6c6f7f5d5fb91bddc7bc0
anandkhatri <akhatri@mobiquityinc.com>
2020-04-01 00:19:53 +0530

    Init

---
+++ src/main/java/com/example/gradlespringhelloworld/GradleSpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradlespringhelloworld/GradleSpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlespringhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
